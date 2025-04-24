09b91ed1a4716eef3dc99972b4cb8a126cfcdf20
ali <alijahed93@yahoo.com>
2025-04-24 18:30:44 +0330

    add product to dom

--- app.js
+++ app.js
@@ -2,15 +2,59 @@ const showCart = document.querySelector(".fa-basket-shopping")
 const cart = document.querySelector(".cart")
 const backdrop = document.querySelector(".backdrop")
 const closeCart = document.querySelector(".close-cart")
+const items = document.querySelector(".items")
 
-showCart.addEventListener('click',()=>{
-    cart.style.opacity= '1';
-    cart.style.transform='translateY(10vh)'
-    backdrop.style.display='block';
+import {productsData} from "./products.js"
+
+showCart.addEventListener('click', () => {
+    cart.style.opacity = '1';
+    cart.style.transform = 'translateY(10vh)'
+    backdrop.style.display = 'block';
 })
-closeCart.addEventListener('click', ()=>{
-    cart.style.opacity= '0';
-    cart.style.transform='translateY(-100vh)'
-    backdrop.style.display='none';
+closeCart.addEventListener('click', () => {
+    cart.style.opacity = '0';
+    cart.style.transform = 'translateY(-100vh)'
+    backdrop.style.display = 'none';
 
 })
+
+class Products {
+    getProducts() {
+        return productsData;
+    }
+}
+
+class UI {
+displayProducts(products){
+    let result = '';
+    products.forEach((item)=>{
+        result+=
+        `
+                <div class="item">
+                <img src=${item.imageUrl} alt="">
+                <div class="details">
+                    <p class="price">${item.price} $</p>
+                    <p class="name">${item.title}</p>
+                </div>
+                <button class="btn" data-id=${item.id}>Add to cart</button>
+            </div>
+        `
+        items.innerHTML = result;
+    })
+
+}
+}
+
+class Storage {
+    static saveProducts(products){
+        localStorage.setItem('products', JSON.stringify(products));
+    }
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const products = new Products();
+    const productsData = products.getProducts();
+    const ui = new UI();
+    ui.displayProducts(productsData);
+    Storage.saveProducts(productsData);
+});
---
+++ products.js
@@ -0,0 +1,44 @@
+export const productsData = [
+    {
+        id:1,
+        title:'item1',
+        price:10,
+        imageUrl: "./assests/Package-PNG-Clipart.png",
+    },
+    {
+        id:2,
+        title:'item2',
+        price:12,
+        imageUrl: "./assests/Package-PNG-Clipart.png",
+    },
+    {
+        id:3,
+        title:'item3',
+        price:13,
+        imageUrl: "./assests/Package-PNG-Clipart.png",
+    },
+    {
+        id:4,
+        title:'item4',
+        price:14,
+        imageUrl: "./assests/Package-PNG-Clipart.png",
+    },
+    {
+        id:5,
+        title:'item5',
+        price:15,
+        imageUrl: "./assests/Package-PNG-Clipart.png",
+    },
+    {
+        id:6,
+        title:'item6',
+        price:16,
+        imageUrl: "./assests/Package-PNG-Clipart.png",
+    },
+    {
+        id:7,
+        title:'item7',
+        price:17,
+        imageUrl: "./assests/Package-PNG-Clipart.png",
+    },
+];
